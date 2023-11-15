e61102db836af03fa6d2adcfb36c14aa1beb7eef
nol4lej <nome.ahumada@outlook.com>
2023-11-14 22:25:27 -0300

    Skills slider for navtab init

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,32 +1,13 @@
-import html from "../../assets/html.svg"
-import css from "../../assets/css.svg"
-import php from "../../assets/php.svg"
+import skills from "./skillsData.js";
 
 class SkillsSection extends HTMLElement{
 
-    skills = {
-        Frontend: [
-            { 
-                title: "HTML",
-                img: html
-            },
-            { 
-                title: "CSS",
-                img: css
-            }
-        ],
-        Backend: [
-            { 
-                title: "PHP",
-                img: php
-            }
-        ],
-        Tools: [
-            { 
-                title: "PHP",
-                img: php
-            }
-        ]
+    skills;
+    currentIndex = 0;
+
+    constructor() {
+        super();
+        this.skills = skills;
     }
     
     connectedCallback(){
@@ -36,19 +17,36 @@ class SkillsSection extends HTMLElement{
     render(){
         this.innerHTML = `
             <nav class="navtab__types__container">
-                <i class="material-icons">chevron_left</i>
-                <ul>
+                <ul id="typeList">
                     ${this.getTypes().map(t => `
                         <li id="${t}" role="button" data-type-list>${t}</li>
                     `).join("")}
                 </ul>
-                <i class="material-icons">chevron_right</i>
+                <i class="material-icons" role="button" id="prev-btn">chevron_left</i>
+                <i class="material-icons" role="button" id="next-btn">chevron_right</i>
             </nav>
-            <section>
+            <section class="navtab__content__container">
                 <article id="navtab-content"></article>
             </section>
         `
         this.openSkill()
+        this.handleSlider()
+    }
+
+    openSkill() {
+        const buttons = this.querySelectorAll("li[data-type-list]");
+      
+        buttons.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                buttons.forEach(b => b.classList.remove("active"));
+                btn.classList.add("active");
+                this.renderContent(btn.id);
+                this.currentIndex = index;
+            });
+        });
+      
+        // Llama a renderContent con el tipo actual al cargar la página
+        this.renderContent(buttons[this.currentIndex].id);
     }
 
     getTypes(){
@@ -56,25 +54,13 @@ class SkillsSection extends HTMLElement{
         return types
     }
 
-    openSkill(){
-        const buttons = this.querySelectorAll("li[data-type-list]")
+    getCurrentIndex() {
+        return this.currentIndex;
+      }
 
-        buttons.forEach(btn => {
-
-            // activo la primera tab
-            buttons[0].classList.add("active")
-            this.renderContent(buttons[0].id)
-
-            btn.addEventListener("click", () => {
-                buttons.forEach(b => b.classList.remove("active"));
-                btn.classList.add("active");
-                this.renderContent(btn.id)
-            })
-        })
-    }
 
     renderContent(id){
-        const container = document.getElementById("navtab-content")
+        const container = this.querySelector("#navtab-content")
         container.innerHTML = `
             ${this.skills[id].map(skill => `
                 <figure>
@@ -85,6 +71,49 @@ class SkillsSection extends HTMLElement{
         `
     }
 
+    handleSlider() {
+        const slidesContainer = document.getElementById("typeList");
+        const slide = document.querySelector("li[data-type-list]");
+        const prevButton = document.getElementById("prev-btn");
+        const nextButton = document.getElementById("next-btn");
+      
+        const recalculateSizes = () => {
+            const slideWidth = slide.clientWidth + 10; // Considera el espacio entre elementos
+            const containerWidth = slidesContainer.clientWidth;
+            const initialScroll = Math.floor((slideWidth / 2) - (containerWidth / 2));
+        
+            slidesContainer.scrollLeft = initialScroll;
+        };
+      
+        // Recalcular los tamaños al cargar la página y al cambiar el tamaño de la ventana
+        window.addEventListener('load', recalculateSizes);
+        window.addEventListener('resize', recalculateSizes);
+      
+        nextButton.addEventListener("click", () => {
+            this.currentIndex = (this.currentIndex + 1) % this.getTypes().length;
+            this.renderContent(this.getTypes()[this.currentIndex]);
+            slidesContainer.scrollLeft += slide.clientWidth + 10; // Considera el espacio entre elementos
+        
+            // Verifica si estás en el último slide y vuelve al primer slide
+            if (this.currentIndex === 0) {
+              // Ajusta el desplazamiento al principio del contenedor
+              slidesContainer.scrollLeft = 0;
+            }
+        });
+      
+        prevButton.addEventListener("click", () => {
+            this.currentIndex = (this.currentIndex - 1 + this.getTypes().length) % this.getTypes().length;
+            this.renderContent(this.getTypes()[this.currentIndex]);
+            slidesContainer.scrollLeft -= slide.clientWidth + 10; // Considera el espacio entre elementos
+        
+            // Verifica si estás en el primer slide y vuelve al último slide
+            if (this.currentIndex === this.getTypes().length - 1) {
+              // Ajusta el desplazamiento al final del contenedor
+              slidesContainer.scrollLeft = slidesContainer.scrollWidth;
+            }
+        });
+    }
+      
     
 }
 
