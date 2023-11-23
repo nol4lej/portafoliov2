6aea9121161e9faaa2b7977b7a580ddc7691b87c
nol4lej <nome.ahumada@outlook.com>
2023-11-22 23:42:38 -0300

    Init - structure done

---
+++ src/components/Projects/ProjectsData.js
@@ -0,0 +1,14 @@
+import loginfirebase from "../../assets/projects/loginfirebase.png"
+
+const ProjectsData = [
+    {
+        title: "Login con Firebase",
+        img : loginfirebase,
+        tech : "Firebase, Webpack, Express, Vercel, JavaScript, CSS, HTML",
+        text : "Arquitectura basada en SPA (Single Page Application) junto con Web Components y autenticación a través de Firebase. Programación basada en eventos y orientada a objetos, modularidad y manejo asíncrono.",
+        codeLink : "https://github.com/nol4lej/login-firebase",
+        deployedLink : "https://login-firebase-omega.vercel.app/"
+    }
+]
+
+export default ProjectsData
