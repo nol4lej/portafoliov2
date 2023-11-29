48613517a8b97b52cbd8a43da690690d1d902b9c
nol4lej <nome.ahumada@outlook.com>
2023-11-28 22:30:35 -0300

    load event added to initialpopup handler

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ import "./src/components/Navbar/Navbar.js"
 import "./src/components/Header/Header.js"
 import "./src/components/Skills/Skills.js"
 import "./src/components/Projects/Projects.js"
+import "./src/components/InitialPopup/InitialPopup.js"
+import "./src/components/Footer/Footer.js"
+
+const root = document.getElementById("root")
 
 window.addEventListener("DOMContentLoaded", () => {
-
-    const root = document.getElementById("root")
-
     root.innerHTML = `
         <nav-bar></nav-bar>
         <header-section></header-section>
         <skills-section></skills-section>
         <projects-section></projects-section>
+        <footer-component></footer-component>
+        <initial-popup></initial-popup>
     `
-
 })
+
+window.addEventListener("load", () => {
+    // const initialPopup = document.createElement("initial-popup");
+    // document.body.appendChild(initialPopup);
+    const initialPopup = document.querySelector("initial-popup");
+    initialPopup.classList.add("active");
+});
