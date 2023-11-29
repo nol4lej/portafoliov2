34b7c7f68e1b30cb0e8842875a4182f2bf8309ae
nol4lej <nome.ahumada@outlook.com>
2023-11-28 22:27:08 -0300

    Techs added like labels - Buttons styles - Tech data changed to array

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,17 +1,67 @@
+import ProjectsData from "./ProjectsData.js"
+
 class ProjectsSection extends HTMLElement{
+
+    Projects;
+
+    constructor(){
+        super()
+        this.Projects = ProjectsData
+    }
 
     connectedCallback(){
         this.render()
+        this.renderContent()
     }
 
     render(){
         this.innerHTML = `
             <h2>Proyectos</h2>
-            <div>
-                
-            </div>
+            <div id="projects__content__container"></div>
         `
     }
+
+    renderContent(){
+        const container = this.querySelector("#projects__content__container")
+        
+        container.innerHTML = `
+            ${this.Projects.map(p => `
+                ${this.cards(p)}
+            `)}
+        `
+    }
+
+    cards({ title, img, tech, text, codeLink, deployedLink }){
+        const card = `
+            <figure class="projects__card__container">
+                <div class="project__card__img__container">
+                    <img src="${img}">
+                </div>
+                <div class="project__card__info__container">
+                    <h3>${title}</h3>
+                    <div class="project__card__tech__container">
+                        ${tech.map(t => `
+                            <span>${t}</span>
+                        `).join("")}
+                    </div>
+                    <p>${text}</p>
+                </div>
+                <div class="project_card__buttons__container">
+                    <a href="${codeLink}" target="_blank">
+                        Fuente
+                        <i class="material-icons">arrow_outward</i>
+                    </a>
+                    <a href="${deployedLink}" target="_blank">
+                        Desplegar
+                        <i class="material-icons">arrow_outward</i>
+                    </a>
+                </div>
+            </figure>
+        `
+        return card
+    }
+
+
 
 
 
--- src/components/Projects/ProjectsData.js
+++ src/components/Projects/ProjectsData.js
@@ -4,8 +4,8 @@ const ProjectsData = [
     {
         title: "Login con Firebase",
         img : loginfirebase,
-        tech : "Firebase, Webpack, Express, Vercel, JavaScript, CSS, HTML",
-        text : "Arquitectura basada en SPA (Single Page Application) junto con Web Components y autenticación a través de Firebase. Programación basada en eventos y orientada a objetos, modularidad y manejo asíncrono.",
+        tech : ["Firebase", "Webpack", "Express", "Vercel", "JavaScript", "CSS", "HTML"],
+        text : "Arquitectura basada en SPA (Single Page Application) junto con Web Components y autenticación a través de Firebase. Programación basada en eventos y orientada a objetos, modularidad, manejo asíncrono y patrón observador.",
         codeLink : "https://github.com/nol4lej/login-firebase",
         deployedLink : "https://login-firebase-omega.vercel.app/"
     }
